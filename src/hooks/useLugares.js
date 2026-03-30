bb98717eebec5f3d4237b9abae65e3ad2c36af2e
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-03-30 00:29:13 -0500

    Organice el hook useLugares para la funcionalidad de el filtrado por municipios

--- src/hooks/useLugares.js
+++ src/hooks/useLugares.js
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
-import { getLugares, deleteLugar } from "../services/lugaresServices";
+import {
+  getLugares,
+  getMunicipios,
+  deleteLugar,
+} from "../services/lugaresServices";
 import { mapLugar } from "../utils/lugaresMapper";
 import Swal from "sweetalert2";
+
 export default function useLugares() {
   const [lugares, setLugares] = useState([]);
+  const [municipios, setMunicipios] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedMunicipioId, setSelectedMunicipioId] = useState("");
+
   const [showModal, setShowModal] = useState(false);
   const [lugarAEliminar, setLugarAEliminar] = useState(null);
+
+  useEffect(() => {
+    cargarMunicipios();
+  }, []);
+
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       cargarLugares();
     }, 500);
     return () => clearTimeout(delayDebounceFn);
-  }, [searchQuery]);
+  }, [searchQuery, selectedMunicipioId]);
+
   const cargarLugares = async () => {
     try {
       setLoading(true);
       const response = await getLugares({
         search: searchQuery || undefined,
+        municipio_id: selectedMunicipioId || undefined,
       });
       const procesados = (response.data || []).map(mapLugar);
       setLugares(procesados);
@@ -34,6 +49,16 @@ export default function useLugares() {
       setLoading(false);
     }
   };
+
+  const cargarMunicipios = async () => {
+    try {
+      const response = await getMunicipios();
+      setMunicipios(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   const eliminar = async () => {
     if (!lugarAEliminar) return;
     try {
@@ -46,12 +71,16 @@ export default function useLugares() {
       setLugarAEliminar(null);
     }
   };
+
   return {
     lugares,
+    municipios,
     loading,
     error,
     searchQuery,
     setSearchQuery,
+    selectedMunicipioId,
+    setSelectedMunicipioId,
     showModal,
     setShowModal,
     setLugarAEliminar,
