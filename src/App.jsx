e2425e3f53a8c5dab21ae7e91e8f7c5dc946aba8
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-03-30 00:11:48 -0500

    HU4 - Cree la ruta de hospedajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Lugares from "./pages/Lugares.jsx";
+import Hospedajes from ".pages/Hospedajes.jsx";
 
 function App() {
-  return <Route path="/lugares" element={<Lugares />} />;
+  return (
+    <Routes>
+      <Route path="/lugares" element={<Lugares />} />;
+      <Route path="/hospedajes" element={<Hospedajes />} />;
+    </Routes>
+  );
 }
 
 export default App;
