7af35c422e5478aa7cdef177d0c16328188f71a0
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-03-30 01:20:23 -0500

    HU6 - Añadi la ruta para ver los detalles de los hospedajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Importacion de componentes
+import VerHospedajes from "./components/ver-hospedajes/VerHospedajes.jsx";
+
+//Importacion de pages
 import Lugares from "./pages/Lugares.jsx";
 import Hospedajes from ".pages/Hospedajes.jsx";
 
@@ -7,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/lugares" element={<Lugares />} />;
       <Route path="/hospedajes" element={<Hospedajes />} />;
+      <Route path="/hospedajes/:id" element={<VerHospedajes />} />
     </Routes>
   );
 }
