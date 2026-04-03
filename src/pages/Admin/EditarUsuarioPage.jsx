df78d7e17ac5036ef00dcba65bce99d49d42ac4c
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-03 13:21:44 -0500

    se creó la pagina que contiene los componentes que forman la interfaz, se creó la ruta para que el admin pueda ingresar a la interfaz y se implementaron los hooks de seguridad para que los usuarios no autenticados no puedan entrar mediante URL

---
+++ src/pages/Admin/EditarUsuarioPage.jsx
@@ -0,0 +1,21 @@
+import EditarUsuario from "../../Components/Admin/AdminUsuarios/EditarUsuario";
+import Header from "../../Components/header/index.jsx";
+import Footer from "../../Components/footer/index.jsx";
+import useAdminRedirect from "../../hooks/UseAdminRedirect.jsx";
+import AuthRedirect from "../../hooks/useAuthRedirect.jsx";
+
+const EditarUsuarioPage = () => {
+  AuthRedirect();
+  useAdminRedirect();
+  return (
+    <div>
+      <Header />
+      <div className="pt-20">
+        <EditarUsuario />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default EditarUsuarioPage;
