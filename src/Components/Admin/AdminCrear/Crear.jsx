ecbaad7facbdb87d4f4f10b3dcb372d05bcf5714
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-03 17:05:34 -0500

    agregué estados del nuevo lugar y logica para cargar los municipios

--- src/Components/Admin/AdminCrear/Crear.jsx
+++ src/Components/Admin/AdminCrear/Crear.jsx
@@ -9,6 +9,38 @@ function CrearUniversal() {
   const { tipo } = useParams();
   const navigate = useNavigate();
 
+  const [municipios, setMunicipios] = useState([]);
+  const [imagenes, setImagenes] = useState([]);
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    descripcion: "",
+    imagenes: "",
+    ubicacion: "",
+    coordenadas: "",
+    municipio_id: "",
+  });
+
+  useEffect(() => {
+    const fetchMunicipios = async () => {
+      try {
+        const response = await axios.get(
+          import.meta.env.VITE_API_URL + "/municipios",
+        );
+        setMunicipios(
+          Array.isArray(response.data.data) ? response.data.data : [],
+        );
+      } catch (error) {
+        console.error(error);
+        setMunicipios([]);
+      }
+    };
+
+    if (tipo === "lugar" || tipo === "hospedaje") {
+      fetchMunicipios();
+    }
+  }, [tipo]);
+
   return (
     <div className="w-full min-h-screen flex items-center justify-evenly py-40 bg-gray-100 px-6">
     </div>
