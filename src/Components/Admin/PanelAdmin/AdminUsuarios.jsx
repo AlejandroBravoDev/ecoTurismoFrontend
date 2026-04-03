0376c8c6c5418e8d0c68b8f0d732f34daac7d56e
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-02 23:43:59 -0500

    creé la estructura base que va a tener el componente

---
+++ src/Components/Admin/PanelAdmin/AdminUsuarios.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import noImage from "../../../assets/noImage.webp";
+
+const AdminUsers = () => {
+  return (
+    <div className="min-h-screen bg-slate-50 flex flex-col items-center w-full">
+      <main className="w-full flex flex-col items-center flex-grow pt-20">
+        
+      </main>
+    </div>
+  );
+};
+
+export default AdminUsers;
