2a22e889e7f5910fa0d3096c3c5097c0d694f3ee
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-03 19:30:18 -0500

    se agregaron los estados y endpoints dinamicos según el tipo que se  quiera editar

--- src/Components/Admin/AdminEditar/EditarUniversal.jsx
+++ src/Components/Admin/AdminEditar/EditarUniversal.jsx
@@ -7,7 +7,24 @@ import { useDropzone } from "react-dropzone";
 
 function Editar() {
   const { tipo, id } = useParams();
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [imagenesExistentes, setImagenesExistentes] = useState([]);
+  const [imagenesNuevas, setImagenesNuevas] = useState([]);
+  const [ubicacion, setUbicacion] = useState("");
+  const [coordenadas, setCoordenadas] = useState("");
+  
+  const endpoints = useMemo(
+    () => ({
+      lugares: `lugares/${id}`,
+      hospedajes: `hospedajes/${id}`,
+      usuario: `usuario/${id}`,
+    }),
+    [id],
+  );
+  
   const navigate = useNavigate();
+  const endpoint = endpoints[tipo];
 
   return (
     <div className="w-full h-full flex flex-col lg:flex-row items-center justify-evenly py-10 lg:py-20 bg-gray-50 gap-10 px-4">
