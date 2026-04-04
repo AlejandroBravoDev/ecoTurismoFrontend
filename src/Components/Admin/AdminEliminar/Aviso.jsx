a56e6d4cc629d66348fab93f6a6965f95b21bd24
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-03 20:20:09 -0500

    se creó la estructura para la alerta de confirmación para el admin

---
+++ src/Components/Admin/AdminEliminar/Aviso.jsx
@@ -0,0 +1,10 @@
+function ConfirmarEliminar({ message, onConfirm, onCancel }) {
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="flex flex-col items-center justify-around bg-white rounded-xl p-6 w-xl h-72 shadow-xl">
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmarEliminar;
