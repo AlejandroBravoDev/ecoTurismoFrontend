f568ef61c86cda649d991f3e942058156a4e9854
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-03 19:27:49 -0500

    estructura e imports

---
+++ src/Components/Admin/AdminEditar/EditarUniversal.jsx
@@ -0,0 +1,18 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState, useCallback, useMemo } from "react";
+import api from "../../services/api";
+import "../panelAdmin/tailwind.css";
+import Swal from "sweetalert2";
+import { useDropzone } from "react-dropzone";
+
+function Editar() {
+  const { tipo, id } = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full h-full flex flex-col lg:flex-row items-center justify-evenly py-10 lg:py-20 bg-gray-50 gap-10 px-4">
+    </div>
+  );
+}
+
+export default Editar;
