a448616be97ef1e16570eb38e29a63bfc564b2f2
Valentina <vr9446691@gmail.com>
2026-04-06 16:55:44 -0500

    fix(HU02): corregir import de VerHospedajes y rutas duplicadas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,46 @@ import ResetPassword from "./pages/ResetPassword.jsx";
 import Login from "./pages/login.jsx";
 import Registro from "./pages/registro.jsx";
 
+import Contacto from "./pages/Contacto";
+import QueOfrecemosPage from "./pages/QueOfrecemos";
+import FaqEcoturismoPage from "./pages/PreguntasFrecuentesPage";
+import Politicas from "./pages/Politicas.jsx";
+//importación de rutas para el admin
+import PanelAdminPage from "./pages/Admin/PanelAdminPage.jsx";
+import AdminUsuariosPage from "./pages/Admin/AdminUsuariosPage.jsx";
+import EditarUsuarioPage from "./pages/Admin/EditarUsuarioPage.jsx";
+import AdminCrearPage from "./pages/Admin/AdminCrearPage.jsx";
+import EditarUniversalPage from "./pages/Admin/EditarUniversalPage.jsx";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/lugares" element={<Lugares />} />;
       <Route path="/lugares/:id" element={<VerLugares />} />
+
       <Route path="/hospedajes" element={<Hospedajes />} />;
       <Route path="/hospedajes/:id" element={<VerHospedajes />} />
+
       <Route path="/perfil" element={<PerfilUser />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/login" element={<Login />} />
       <Route path="/registro" element={<Registro />} />
+
+      <Route path="/ofrecemos" element={<QueOfrecemosPage />} />
+      <Route path="/preguntasFrecuentes" element={<FaqEcoturismoPage />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/politicas" element={<Politicas />} />
+      {/* Rutas del admin */}
+      <Route path="/PanelAdmin" element={<PanelAdminPage />} />
+      <Route path="/PanelAdmin/usuarios" element={<AdminUsuariosPage />} />
+      <Route path="/PanelAdmin/usuarios/:id" element={<EditarUsuarioPage />} />
+      <Route path="/PanelAdmin/crear/:tipo" element={<AdminCrearPage />} />
+      <Route
+        path="/PanelAdmin/editar/:tipo/:id"
+        element={<EditarUniversalPage />}
+      />
     </Routes>
   );
 }
